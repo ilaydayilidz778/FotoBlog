823c21b7c6a00f1b62c8eb139d56aa2e8e312b3e
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-01-25 03:23:42 +0300

    The project has been developed using the MVC architecture, CRUD operations, and the Entity Framework for the database. In addition to the functionality of adding posts, deletion, and update operations have also been incorporated into the project. Furthermore, specific view models have been created for the update and delete processes, enhancing the project with additional visual elements.

--- FotoBlog/Controllers/GonderilerController.cs
+++ FotoBlog/Controllers/GonderilerController.cs
@@ -1,6 +1,8 @@
 ﻿using FotoBlog.Data;
 using FotoBlog.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace FotoBlog.Controllers
 {
@@ -8,6 +10,7 @@ namespace FotoBlog.Controllers
     {
         private readonly UygulamaDbContext _db;
         private readonly IWebHostEnvironment _env;
+        private readonly object _logger;
 
         public GonderilerController(UygulamaDbContext db, IWebHostEnvironment env)
         {
@@ -44,6 +47,116 @@ namespace FotoBlog.Controllers
             }
 
             return View(vm);
+        }
+        public IActionResult Duzenle(int id)
+        {
+            Gonderi duzenlenecekGonderi = _db.Gonderiler.FirstOrDefault(g => g.Id == id)!;
+            if (duzenlenecekGonderi == null)
+            {
+                return NotFound();
+            }
+
+            DuzenleGonderiViewModel duzenleViewModel = new DuzenleGonderiViewModel
+            {
+                Id = duzenlenecekGonderi.Id,
+                Baslik = duzenlenecekGonderi.Baslik,
+                ResimYolu = duzenlenecekGonderi.ResimYolu
+            };
+
+            return View(duzenleViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Duzenle(DuzenleGonderiViewModel duzenleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Gonderi duzenlenecekGonderi = _db.Gonderiler.FirstOrDefault(g => g.Id == duzenleViewModel.Id)!;
+                if (duzenlenecekGonderi != null)
+                {
+                    duzenlenecekGonderi.Id = duzenleViewModel.Id;
+                    duzenlenecekGonderi.Baslik = duzenleViewModel.Baslik;
+
+                    if (duzenleViewModel.ResimYolu != null)
+                    {
+                        duzenlenecekGonderi.ResimYolu = duzenleViewModel.ResimYolu;
+                        if (Path.GetFileName(duzenlenecekGonderi.ResimYolu) != duzenleViewModel.Resim.FileName)
+                        {
+                            // Resim değişmişse ve eski resim başka bir gönderi tarafından kullanılmıyorsa kaldırız.
+                            ResmiKlasördenKaldır(duzenlenecekGonderi);
+
+                            // Yeni resmi ekle
+                            string ext = Path.GetExtension(duzenleViewModel.Resim.FileName);
+                            string yeniDosyaAdi = Guid.NewGuid() + ext;
+                            string yol = Path.Combine(_env.WebRootPath, "img", "upload", yeniDosyaAdi);
+
+                            using (var fs = new FileStream(yol, FileMode.CreateNew))
+                            {
+                                duzenleViewModel.Resim.CopyTo(fs);
+                            }
+
+                            duzenlenecekGonderi.ResimYolu = yeniDosyaAdi;
+                        }
+                    }
+                    _db.SaveChanges();
+                    return RedirectToAction("Index", "Home", new { Sonuc = "Duzenlendi" });
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            return View(duzenleViewModel);
+        }
+
+        public IActionResult Sil(int id)
+        {
+            Gonderi silinecekGonderi = _db.Gonderiler.FirstOrDefault(g => g.Id == id)!;
+            if (silinecekGonderi == null)
+            {
+                return NotFound();
+            }
+
+            return View(silinecekGonderi);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Sil(Gonderi silinecekGonderi)
+        {
+            if (ModelState.IsValid)
+            {
+                // Gönderi silinirken resim bir başka gönderi tarafından kullnamıyorsa silinir.
+                ResmiKlasördenKaldır(silinecekGonderi);
+
+                _db.Gonderiler.Remove(silinecekGonderi);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home", new { Sonuc = "Silindi" });
+            }
+
+            return View(silinecekGonderi);
+        }
+
+        public void ResmiKlasördenKaldır(Gonderi gonderi)
+        {
+            if (gonderi.ResimYolu != null)
+            {
+                string silincekResimDosyaAdi = gonderi.ResimYolu;
+                string dosyaYolu = Path.Combine(_env.WebRootPath, "img", "upload", silincekResimDosyaAdi);
+
+
+                if (System.IO.File.Exists(dosyaYolu))
+                {
+                    bool baskabirGonderiTarafindanKullaniliyorMu = _db.Gonderiler.Any(g => g.ResimYolu == gonderi.ResimYolu && g.Id != gonderi.Id);
+
+                    if (!baskabirGonderiTarafindanKullaniliyorMu)
+                    {
+                        System.IO.File.Delete(dosyaYolu);
+                    }
+                }
+            }
         }
     }
 }
--- FotoBlog/Models/DuzenleGonderiViewModel.cs
+++ FotoBlog/Models/DuzenleGonderiViewModel.cs
@@ -3,16 +3,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FotoBlog.Models
 {
-    public class YeniGonderiVewModel
+    public class DuzenleGonderiViewModel
     {
+        public int Id { get; set; }
 
         [Display(Name = "Başlık")]
         [Required(ErrorMessage = "{0} alanı zorunludur!")]
         public string Baslik { get; set; } = null!;
 
-        [Display(Name = "Resim")]
         [Required(ErrorMessage = "{0} koyulması zorunludur!")]
+        public string ResimYolu { get; set; } = null!;
+
+        [Display(Name = "Farklı Bir Resim Ekle")]
         [GecerliResim(MaxDosyaBoyutuMB = 1.2)]
-        public IFormFile Resim { get; set; } = null!;
+        public IFormFile? Resim { get; set; } = null!;
     }
 }
+
+
+
+
